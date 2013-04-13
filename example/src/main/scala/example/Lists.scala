000ff0def2a042ee8f1d1b53249bf7e3ba9007d6
Rahul Revo <rahulrevo@gmail.com>
2013-04-13 12:24:15 -0700

    List sum and max implementations

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -1,42 +1,53 @@
 package example
 
-import common._
+import java.util.NoSuchElementException
+
+import scala.annotation.tailrec
 
 object Lists {
   /**
-   * This method computes the sum of all elements in the list xs. There are
-   * multiple techniques that can be used for implementing this method, and
-   * you will learn during the class.
-   *
-   * For this example assignment you can use the following methods in class
-   * `List`:
-   *
-   *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
-   *  - `xs.head: Int` returns the head element of the list `xs`. If the list
-   *    is empty an exception is thrown
-   *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
-   *    list `xs` without its `head` element
-   *
-   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
-   *  solution.
+   * This method computes the sum of all elements in the list xs.
    *
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = {
+
+    @tailrec
+    def sum(acc: Int, xs: List[Int]): Int = {
+      if (xs.isEmpty)
+        acc
+      else
+        sum(acc + xs.head, xs.tail)
+    }
+    sum(0, xs);
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
    * list `xs` is empty it throws a `java.util.NoSuchElementException`.
    *
-   * You can use the same methods of the class `List` as mentioned above.
-   *
-   * ''Hint:'' Again, think of a recursive solution instead of using looping
-   * constructs. You might need to define an auxiliary method.
-   *
    * @param xs A list of natural numbers
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+
+    @tailrec
+    def max(currentMax: Int, xs: List[Int]): Int = {
+      if (xs.isEmpty)
+        currentMax
+      else {
+        if (currentMax > xs.head)
+          max(currentMax, xs.tail)
+        else
+          max(xs.head, xs.tail)
+      }
+    }
+
+    if (xs.isEmpty)
+      throw new NoSuchElementException
+    else
+      max(xs.head, xs.tail)
+  }
 }
